c9591b8d8d6c03aba38c6b77cc0df1c1c3001096
Mayank Khanna <khanna98@outlook.com>
2021-03-21 23:51:12 +0530

    Minor Bug Fix - Inventory Edit

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json')
-const morgan = require('morgan')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const morgan = require("morgan");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(morgan('tiny'))
-app.use(express.static("client/build"))
+app.use(morgan("tiny"));
+app.use(express.static("client/build"));
 
-const keys = require('./config/keys');
-const mongoose = require('mongoose');
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
 // Connecting to the Database
-mongoose.connect(keys.mongoURI, {
+mongoose
+  .connect(keys.mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Successfully connected to Database");
-}).catch(err => {
-    console.log('Could not connect to the Database. Exiting now....', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the Database. Exiting now....", err);
     process.exit();
-})
-
+  });
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -38,15 +40,20 @@ mongoose.connect(keys.mongoURI, {
 //     })
 // })
 
-require('./routes/product.route.js')(app)
-require('./routes/sale.route.js')(app)
-require('./routes/purchase.route.js')(app)
-require('./routes/user.js')(app)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+require("./routes/product.route.js")(app);
+require("./routes/sale.route.js")(app);
+require("./routes/purchase.route.js")(app);
+require("./routes/user.js")(app);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/client/build/index.html"))
+// })
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`App is listening on PORT - ${process.env.PORT}`)  
-})
+  console.log(`App is listening on PORT - ${process.env.PORT}`);
+});
