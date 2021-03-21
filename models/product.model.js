0329d877bb6a025554066a27b8c9180e2fbd3b2f
Mayank Khanna <khanna98@outlook.com>
2021-03-21 23:33:02 +0530

    Updated All the models

--- models/product.model.js
+++ models/product.model.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const transactionsQty = {
-    quantity: Number,
-    invoiceNumber: String,
-    dateOfTransaction: Date
-}
+  quantity: Number,
+  invoiceNumber: String,
+  dateOfTransaction: Date,
+  typeOfEntry: String,
+};
 
-const ProductSchema = mongoose.Schema({
+const ProductSchema = mongoose.Schema(
+  {
     title: String,
     productUID: String,
     totalQuantity: Number,
@@ -14,10 +16,11 @@ const ProductSchema = mongoose.Schema({
     dateOfRecieve: Date,
     dateOfInvoice: Date,
     transactions: [transactionsQty],
-    vendorName: String
-}, {
-    timestamps: true
-});
+    vendorName: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Product', ProductSchema);
-
+module.exports = mongoose.model("Product", ProductSchema);
